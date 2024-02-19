5465f609023ce460d59ec19a69be59ddd77392a4
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-19 16:07:34 +0530

    loops

---
+++ arrays-loops/app.js
@@ -0,0 +1,21 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// document.getElementById("demo").innerHTML = fruits.toString(); 
+
+
+const a = ["A", "B", "C", "D"];
+
+console.log(a);
+console.log(a.length);
+
+a.push("E");
+a.push("F");
+
+console.log(a);
+
+const array = [
+    ["A", "B"],
+    [1, 2, 3]
+]
+
+console.log(array[1][2]);

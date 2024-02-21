805114c15df30331a9dd1b542158e2d42d80e560
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-21 21:34:02 +0530

    dom

--- dom-manipulation-and-events/app.js
+++ dom-manipulation-and-events/app.js
@@ -32,16 +32,30 @@
 
 
 
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
-btn.addEventListener('click', function (e) {
-    console.log(e);
+// btn.addEventListener('click', function (e) {
+//     console.log(e);
+// });
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e.target);
+// });
+
+// btn.addEventListener('click', function (e) {
+//     e.target.style.background = 'blue';
+// });
+
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
 });
-
-
-
-
-
-
-
-
